133d05107bf16ea5b24f00fabf5ffbea65b58f4a
Nelson Medel <nmedelb@gmail.com>
2023-05-24 09:33:51 -0400

    Primer commt

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * $proyect_name
+ *
+ * @author El Piero tiene sueño
+ * @version 0.0.1
+ * @since 28-06-2022
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
